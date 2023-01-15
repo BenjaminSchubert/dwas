BenjaminSchubert/wast

Refactor to allow parametrizing steps before and after registration
Also introduce a helper to help parametrizing multiple single value
parameters

src/wast/_pipeline.py
@@ -6,9 +6,13 @@ from concurrent import futures
 from contextvars import ContextVar
 from datetime import datetime, timedelta
 from subprocess import CalledProcessError
-from typing import TYPE_CHECKING, Dict, List, Optional, Tuple
+from typing import Dict, Generator, List, Optional, Tuple
 
 from colorama import Fore, Style
+
+from wast._steps.handlers import BaseStepHandler, StepGroupHandler, StepHandler
+from wast._steps.parametrize import extract_parameters
+from wast._steps.steps import Step
 
 from ._config import Config
 from ._exceptions import (
@@ -21,9 +25,6 @@ from ._exceptions import (
 from ._log_capture import PipePlexer
 from ._logging import set_context_handler
 from ._subproc import set_subprocess_default_pipes
-
-if TYPE_CHECKING:
-    from ._steps.handlers import BaseStepHandler
 
 LOGGER = logging.getLogger(__name__)
 
@@ -50,15 +51,82 @@ class ExceptionWithTimeSpentException(Exception):
 class Pipeline:
     def __init__(self, config: Config) -> None:
         self.config = config
-        self._steps: Dict[str, "BaseStepHandler"] = {}
 
-    def register_step(self, step: "BaseStepHandler") -> None:
-        if step.name in self._steps:
-            raise DuplicateStepException(step.name)
+        self._registered_steps: List[Tuple[str, Step]] = []
+        self._registered_step_groups: List[
+            Tuple[str, List[str], Optional[bool]]
+        ] = []
+        self._steps_cache: Optional[Dict[str, BaseStepHandler]] = None
 
-        self._steps[step.name] = step
+    @property
+    def _steps(self) -> Dict[str, BaseStepHandler]:
+        if self._steps_cache is None:
+            self._steps_cache = self._resolve_steps()
+        return self._steps_cache
 
-    def _resolve_steps(
+    def register_step(self, name: str, step: "Step") -> None:
+        self._registered_steps.append((name, step))
+
+    def register_step_group(
+        self,
+        name: str,
+        requires: List[str],
+        run_by_default: Optional[bool] = None,
+    ) -> None:
+        self._registered_step_groups.append((name, requires, run_by_default))
+
+    def _resolve_steps(self) -> Dict[str, BaseStepHandler]:
+        steps = {}
+
+        for name, func in self._registered_steps:
+            for step in self._resolve_parameters(name, func):
+                if step.name in steps:
+                    raise DuplicateStepException(step.name)
+
+                steps[step.name] = step
+
+        for name, requires, run_by_default in self._registered_step_groups:
+            if name in steps:
+                raise DuplicateStepException(name)
+
+            steps[name] = StepGroupHandler(
+                name, self, requires, run_by_default
+            )
+
+        return steps
+
+    def _resolve_parameters(
+        self, name: str, func: Step
+    ) -> Generator[BaseStepHandler, None, None]:
+        parameters = extract_parameters(func)
+        all_run_by_default = True
+        all_created = []
+
+        for params_id, args in parameters:
+            step_name = ""
+            if len(parameters) > 1 and params_id != "":
+                step_name = f"[{params_id}]"
+
+            step_name = f"{name}{step_name}"
+            current_run_by_default = args.get("run_by_default", None)
+
+            all_created.append(step_name)
+            all_run_by_default = all_run_by_default and current_run_by_default
+
+            yield StepHandler(
+                name=step_name,
+                func=func,
+                pipeline=self,
+                python=args.get("python", None),
+                requires=args.get("requires", None),
+                run_by_default=current_run_by_default,
+                parameters=args,
+            )
+
+        if len(parameters) > 1:
+            yield StepGroupHandler(name, self, all_created, all_run_by_default)
+
+    def _resolve_execution_order(
         self,
         steps: Optional[List[str]] = None,
         only_steps: Optional[List[str]] = None,
@@ -131,7 +199,7 @@ class Pipeline:
         # pylint: disable=too-many-branches,too-many-locals
         start_time = datetime.now()
 
-        steps = self._resolve_steps(steps, only_steps, except_steps)
+        steps = self._resolve_execution_order(steps, only_steps, except_steps)
         if only_steps is None:
             only_steps = steps
         if except_steps is None:
@@ -219,7 +287,7 @@ class Pipeline:
 
             self._log_summary(graph, results, start_time)
 
-    def get_requirement(self, requirement: str) -> "BaseStepHandler":
+    def get_requirement(self, requirement: str) -> BaseStepHandler:
         return self._steps[requirement]
 
     def list_all_steps(
@@ -229,8 +297,10 @@ class Pipeline:
         except_steps: Optional[List[str]] = None,
         show_dependencies: bool = False,
     ) -> None:
-        all_steps = self._resolve_steps(list(self._steps.keys()))
-        selected_steps = self._resolve_steps(steps, only_steps, except_steps)
+        all_steps = self._resolve_execution_order(list(self._steps.keys()))
+        selected_steps = self._resolve_execution_order(
+            steps, only_steps, except_steps
+        )
 
         LOGGER.info("Available steps (* means selected, - means skipped):")
         for step in sorted(all_steps):

src/wast/_steps/parametrize.py
@@ -16,8 +16,8 @@ from .._exceptions import BaseWastException
 
 T = TypeVar("T", bound=Callable[..., Any])
 
-_DEFAULTS = "_wast_defaults"
-_PARAMETERS = "_wast_parameters"
+_DEFAULTS = "__wast_defaults__"
+_PARAMETERS = "__wast_parameters__"
 
 
 class ParameterConflictException(BaseWastException):
@@ -273,6 +273,41 @@ def set_defaults(values: Dict[str, Any]) -> Callable[[T], T]:
         return func
 
     return _apply
+
+
+def build_parameters(**kwargs: Any) -> Callable[[T], T]:
+    """
+    Generate a :py:func:`parametrize` call based on the provided parameters.
+
+    This is a shortcut to build a single :py:func:`parametrize` call, for all
+    non-:python:`None` values that are passed in.
+
+    It will only pass the arguments as a single entry, so this will only ever
+    generate a single entry.
+
+    This is basically a shortcut for:
+
+    .. code-block::
+
+        for key, value in parameters.items():
+            if value is not None:
+                func = parametrize(key, [value])(func)
+
+    :param kwargs: Any key/value pair to pass as a parametrize argument
+    :return: A function to apply the parameters on the given step.
+    """
+    names = []
+    values = []
+
+    for key, value in kwargs.items():
+        if value is not None:
+            names.append(key)
+            values.append(value)
+
+    if names:
+        return parametrize(names, [values])
+
+    return lambda t: t
 
 
 def extract_parameters(

src/wast/predefined/_pytest.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Optional, Sequence, TypeVar
+from typing import Any, Dict, List, Optional, Sequence
 
 # XXX: All imports here should be done from the top level. If we need it,
 #      users might need it
@@ -8,7 +8,6 @@ from .. import Step, StepHandler
 from .. import parametrize as apply_parameters
 from .. import register_managed_step, set_defaults
 
-T = TypeVar("T")
 LOGGER = logging.getLogger(__name__)
 
 
@@ -38,12 +37,11 @@ def pytest(
     *,
     name: Optional[str] = None,
     args: Optional[Sequence[str]] = None,
-    parametrize: Optional[Callable[[Step], Step]] = None,
     python: Optional[str] = None,
     requires: Optional[List[str]] = None,
     dependencies: Optional[Sequence[str]] = None,
     run_by_default: Optional[bool] = None,
-) -> None:
+) -> Step:
     """
     Run `pytest`_.
 
@@ -51,8 +49,6 @@ def pytest(
                  Defaults to :python:`"pytest"`.
     :param args: arguments to pass to the ``pytest`` invocation.
                  Defaults to :python:`[]`.
-    :param parametrize: A :py:func:`wast.parametrize` invocation to apply on
-                        the step.
     :param python: The version of python to use.
                    Defaults to the version *wast* was installed with.
     :param requires: A list of other steps that this step would require.
@@ -60,10 +56,7 @@ def pytest(
                          Defaults to :python:`["pytest"]`.
     :param run_by_default: Whether to run this step by default or not.
                            If :python:`None`, will default to :python:`True`.
-
-    .. seealso::
-
-        How to parametrize methods with :py:func:`wast.parametrize`
+    :return: The step so that you can add additional parameters to it if needed.
 
     .. tip::
 
@@ -89,10 +82,11 @@ def pytest(
             # for the tests
             wast.predefined.package()
 
-            wast.predefined.pytest(
-                dependencies=["pytest", "pytest-cov"],
-                requires=["package"],
-                parametrize=wast.parametrize("python", ["3.8", "3.9", "3.10"])
+            wast.parametrize("python", ["3.8", "3.9", "3.10"])(
+                wast.predefined.pytest(
+                    dependencies=["pytest", "pytest-cov"],
+                    requires=["package"],
+                )
             )
 
         Leveraging :py:func:`wast.parametrize`, this generates 4 different
@@ -105,18 +99,12 @@ def pytest(
 
         .. code-block::
 
-            wast.predefined.pytest(
-                requires=["package"],
-                parametrize=wast.parametrize(
-                    "python", ["3.8", "3.9", "3.10"]
-                )(wast.parametrize(
+            wast.parametrize("python", ["3.8", "3.9", "3.10"])(
+                wast.parametrize(
                     "dependencies",
-                    [
-                        ["pytest", "django==3.0"],
-                        ["pytest", "django==4.0"],
-                    ],
+                    [["pytest", "django==3.0"], ["pytest", "django==4.0"]],
                     ids=["django3", "django4"],
-                )
+                )(wast.predefined.pytest(requires=["package"])
             )
     """
     pytest_ = Pytest()
@@ -124,10 +112,7 @@ def pytest(
     if args is not None:
         pytest_ = apply_parameters("args", [args])(pytest_)
 
-    if parametrize is not None:
-        pytest_ = parametrize(pytest_)  # type: ignore
-
-    register_managed_step(
+    return register_managed_step(
         pytest_,
         dependencies,
         name=name,

src/wast/predefined/_sphinx.py
@@ -1,9 +1,13 @@
 from pathlib import Path
-from typing import Callable, List, Optional, Union
+from typing import List, Optional, Union
 
-from .. import Step, StepHandler
-from .. import parametrize as apply_parameters
-from .. import register_managed_step, set_defaults
+from .. import (
+    Step,
+    StepHandler,
+    build_parameters,
+    register_managed_step,
+    set_defaults,
+)
 
 
 @set_defaults(
@@ -62,12 +66,11 @@ def sphinx(
     sourcedir: Optional[Union[Path, str]] = None,
     output: Optional[Union[Path, str]] = None,
     warning_as_error: Optional[bool] = None,
-    parametrize: Optional[Callable[[Step], Step]] = None,
     python: Optional[str] = None,
     requires: Optional[List[str]] = None,
     dependencies: Optional[List[str]] = None,
     run_by_default: Optional[bool] = None,
-) -> None:
+) -> Step:
     """
     Run `sphinx`_.
 
@@ -82,8 +85,6 @@ def sphinx(
                    Defaults to :python:`None`.
     :param warning_as_error: Turn warnings into errors
                              Defaults to :python:`False`.
-    :param parametrize: A :py:func:`wast.parametrize` invocation to apply on the
-                        step.
     :param python: The version of python to use.
                    Defaults to the version *wast* was installed with.
     :param requires: A list of other steps that this step would require.
@@ -91,6 +92,7 @@ def sphinx(
                          Defaults to :python:`["pytest"]`.
     :param run_by_default: Whether to run this step by default or not.
                            If :python:`None`, will default to :python:`True`.
+    :return: The step so that you can add additional parameters to it if needed.
 
     :Examples:
 
@@ -110,35 +112,27 @@ def sphinx(
 
         .. code-block::
 
-            wast.predefined.sphinx(
-                requires=["package"],
-                parametrize=wast.parametrize(
-                    ("builder", "output"),
-                    [
-                        ("html", "_build/docs"),
-                        # We don't care about the output of those two here.
-                        ("linkcheck", None),
-                        ("doctests", None),
-                    ],
-                    ids=["html", "linkcheck", "doctests"],
-                )
-            )
+            wast.parametrize(
+                ("builder", "output"),
+                [
+                    ("html", "_build/docs"),
+                    # We don't care about the output of those two here.
+                    ("linkcheck", None),
+                    ("doctests", None),
+                ],
+                ids=["html", "linkcheck", "doctests"],
+            )(wast.predefined.sphinx(requires=["package"]))
     """
     sphinx_ = Sphinx()
 
-    for parameter, value in {
-        "builder": builder,
-        "sourcedir": sourcedir,
-        "output": output,
-        "warning_as_error": warning_as_error,
-    }.items():
-        if value is not None:
-            sphinx_ = apply_parameters(parameter, [value])(sphinx_)
+    sphinx_ = build_parameters(
+        builder=builder,
+        sourcedir=sourcedir,
+        output=output,
+        warning_as_error=warning_as_error,
+    )(sphinx_)
 
-    if parametrize is not None:
-        sphinx_ = parametrize(sphinx_)  # type: ignore
-
-    register_managed_step(
+    return register_managed_step(
         sphinx_,
         dependencies,
         name=name,

BenjaminSchubert/dwas

src/dwas/_pipeline.py
@@ -6,9 +6,13 @@ from concurrent import futures
 from contextvars import ContextVar
 from datetime import datetime, timedelta
 from subprocess import CalledProcessError
-from typing import TYPE_CHECKING, Dict, List, Optional, Tuple
+from typing import Dict, Generator, List, Optional, Tuple
 
 from colorama import Fore, Style
+
+from dwas._steps.handlers import BaseStepHandler, StepGroupHandler, StepHandler
+from dwas._steps.parametrize import extract_parameters
+from dwas._steps.steps import Step
 
 from ._config import Config
 from ._exceptions import (
@@ -21,9 +25,6 @@ from ._exceptions import (
 from ._log_capture import PipePlexer
 from ._logging import set_context_handler
 from ._subproc import set_subprocess_default_pipes
-
-if TYPE_CHECKING:
-    from ._steps.handlers import BaseStepHandler
 
 LOGGER = logging.getLogger(__name__)
 
@@ -50,15 +51,82 @@ class ExceptionWithTimeSpentException(Exception):
 class Pipeline:
     def __init__(self, config: Config) -> None:
         self.config = config
-        self._steps: Dict[str, "BaseStepHandler"] = {}
 
-    def register_step(self, step: "BaseStepHandler") -> None:
-        if step.name in self._steps:
-            raise DuplicateStepException(step.name)
+        self._registered_steps: List[Tuple[str, Step]] = []
+        self._registered_step_groups: List[
+            Tuple[str, List[str], Optional[bool]]
+        ] = []
+        self._steps_cache: Optional[Dict[str, BaseStepHandler]] = None
 
-        self._steps[step.name] = step
+    @property
+    def _steps(self) -> Dict[str, BaseStepHandler]:
+        if self._steps_cache is None:
+            self._steps_cache = self._resolve_steps()
+        return self._steps_cache
 
-    def _resolve_steps(
+    def register_step(self, name: str, step: "Step") -> None:
+        self._registered_steps.append((name, step))
+
+    def register_step_group(
+        self,
+        name: str,
+        requires: List[str],
+        run_by_default: Optional[bool] = None,
+    ) -> None:
+        self._registered_step_groups.append((name, requires, run_by_default))
+
+    def _resolve_steps(self) -> Dict[str, BaseStepHandler]:
+        steps = {}
+
+        for name, func in self._registered_steps:
+            for step in self._resolve_parameters(name, func):
+                if step.name in steps:
+                    raise DuplicateStepException(step.name)
+
+                steps[step.name] = step
+
+        for name, requires, run_by_default in self._registered_step_groups:
+            if name in steps:
+                raise DuplicateStepException(name)
+
+            steps[name] = StepGroupHandler(
+                name, self, requires, run_by_default
+            )
+
+        return steps
+
+    def _resolve_parameters(
+        self, name: str, func: Step
+    ) -> Generator[BaseStepHandler, None, None]:
+        parameters = extract_parameters(func)
+        all_run_by_default = True
+        all_created = []
+
+        for params_id, args in parameters:
+            step_name = ""
+            if len(parameters) > 1 and params_id != "":
+                step_name = f"[{params_id}]"
+
+            step_name = f"{name}{step_name}"
+            current_run_by_default = args.get("run_by_default", None)
+
+            all_created.append(step_name)
+            all_run_by_default = all_run_by_default and current_run_by_default
+
+            yield StepHandler(
+                name=step_name,
+                func=func,
+                pipeline=self,
+                python=args.get("python", None),
+                requires=args.get("requires", None),
+                run_by_default=current_run_by_default,
+                parameters=args,
+            )
+
+        if len(parameters) > 1:
+            yield StepGroupHandler(name, self, all_created, all_run_by_default)
+
+    def _resolve_execution_order(
         self,
         steps: Optional[List[str]] = None,
         only_steps: Optional[List[str]] = None,
@@ -131,7 +199,7 @@ class Pipeline:
         # pylint: disable=too-many-branches,too-many-locals
         start_time = datetime.now()
 
-        steps = self._resolve_steps(steps, only_steps, except_steps)
+        steps = self._resolve_execution_order(steps, only_steps, except_steps)
         if only_steps is None:
             only_steps = steps
         if except_steps is None:
@@ -219,7 +287,7 @@ class Pipeline:
 
             self._log_summary(graph, results, start_time)
 
-    def get_requirement(self, requirement: str) -> "BaseStepHandler":
+    def get_requirement(self, requirement: str) -> BaseStepHandler:
         return self._steps[requirement]
 
     def list_all_steps(
@@ -229,8 +297,10 @@ class Pipeline:
         except_steps: Optional[List[str]] = None,
         show_dependencies: bool = False,
     ) -> None:
-        all_steps = self._resolve_steps(list(self._steps.keys()))
-        selected_steps = self._resolve_steps(steps, only_steps, except_steps)
+        all_steps = self._resolve_execution_order(list(self._steps.keys()))
+        selected_steps = self._resolve_execution_order(
+            steps, only_steps, except_steps
+        )
 
         LOGGER.info("Available steps (* means selected, - means skipped):")
         for step in sorted(all_steps):

src/dwas/_steps/parametrize.py
@@ -16,8 +16,8 @@ from .._exceptions import BaseDwasException
 
 T = TypeVar("T", bound=Callable[..., Any])
 
-_DEFAULTS = "_dwas_defaults"
-_PARAMETERS = "_dwas_parameters"
+_DEFAULTS = "__dwas_defaults__"
+_PARAMETERS = "__dwas_parameters__"
 
 
 class ParameterConflictException(BaseDwasException):
@@ -273,6 +273,41 @@ def set_defaults(values: Dict[str, Any]) -> Callable[[T], T]:
         return func
 
     return _apply
+
+
+def build_parameters(**kwargs: Any) -> Callable[[T], T]:
+    """
+    Generate a :py:func:`parametrize` call based on the provided parameters.
+
+    This is a shortcut to build a single :py:func:`parametrize` call, for all
+    non-:python:`None` values that are passed in.
+
+    It will only pass the arguments as a single entry, so this will only ever
+    generate a single entry.
+
+    This is basically a shortcut for:
+
+    .. code-block::
+
+        for key, value in parameters.items():
+            if value is not None:
+                func = parametrize(key, [value])(func)
+
+    :param kwargs: Any key/value pair to pass as a parametrize argument
+    :return: A function to apply the parameters on the given step.
+    """
+    names = []
+    values = []
+
+    for key, value in kwargs.items():
+        if value is not None:
+            names.append(key)
+            values.append(value)
+
+    if names:
+        return parametrize(names, [values])
+
+    return lambda t: t
 
 
 def extract_parameters(

src/dwas/predefined/_pytest.py
@@ -1,6 +1,6 @@
 import logging
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Optional, Sequence, TypeVar
+from typing import Any, Dict, List, Optional, Sequence
 
 # XXX: All imports here should be done from the top level. If we need it,
 #      users might need it
@@ -8,7 +8,6 @@ from .. import Step, StepHandler
 from .. import parametrize as apply_parameters
 from .. import register_managed_step, set_defaults
 
-T = TypeVar("T")
 LOGGER = logging.getLogger(__name__)
 
 
@@ -38,12 +37,11 @@ def pytest(
     *,
     name: Optional[str] = None,
     args: Optional[Sequence[str]] = None,
-    parametrize: Optional[Callable[[Step], Step]] = None,
     python: Optional[str] = None,
     requires: Optional[List[str]] = None,
     dependencies: Optional[Sequence[str]] = None,
     run_by_default: Optional[bool] = None,
-) -> None:
+) -> Step:
     """
     Run `pytest`_.
 
@@ -51,8 +49,6 @@ def pytest(
                  Defaults to :python:`"pytest"`.
     :param args: arguments to pass to the ``pytest`` invocation.
                  Defaults to :python:`[]`.
-    :param parametrize: A :py:func:`dwas.parametrize` invocation to apply on
-                        the step.
     :param python: The version of python to use.
                    Defaults to the version *dwas* was installed with.
     :param requires: A list of other steps that this step would require.
@@ -60,10 +56,7 @@ def pytest(
                          Defaults to :python:`["pytest"]`.
     :param run_by_default: Whether to run this step by default or not.
                            If :python:`None`, will default to :python:`True`.
-
-    .. seealso::
-
-        How to parametrize methods with :py:func:`dwas.parametrize`
+    :return: The step so that you can add additional parameters to it if needed.
 
     .. tip::
 
@@ -89,10 +82,11 @@ def pytest(
             # for the tests
             dwas.predefined.package()
 
-            dwas.predefined.pytest(
-                dependencies=["pytest", "pytest-cov"],
-                requires=["package"],
-                parametrize=dwas.parametrize("python", ["3.8", "3.9", "3.10"])
+            dwas.parametrize("python", ["3.8", "3.9", "3.10"])(
+                dwas.predefined.pytest(
+                    dependencies=["pytest", "pytest-cov"],
+                    requires=["package"],
+                )
             )
 
         Leveraging :py:func:`dwas.parametrize`, this generates 4 different
@@ -105,18 +99,12 @@ def pytest(
 
         .. code-block::
 
-            dwas.predefined.pytest(
-                requires=["package"],
-                parametrize=dwas.parametrize(
-                    "python", ["3.8", "3.9", "3.10"]
-                )(dwas.parametrize(
+            dwas.parametrize("python", ["3.8", "3.9", "3.10"])(
+                dwas.parametrize(
                     "dependencies",
-                    [
-                        ["pytest", "django==3.0"],
-                        ["pytest", "django==4.0"],
-                    ],
+                    [["pytest", "django==3.0"], ["pytest", "django==4.0"]],
                     ids=["django3", "django4"],
-                )
+                )(dwas.predefined.pytest(requires=["package"])
             )
     """
     pytest_ = Pytest()
@@ -124,10 +112,7 @@ def pytest(
     if args is not None:
         pytest_ = apply_parameters("args", [args])(pytest_)
 
-    if parametrize is not None:
-        pytest_ = parametrize(pytest_)  # type: ignore
-
-    register_managed_step(
+    return register_managed_step(
         pytest_,
         dependencies,
         name=name,

src/dwas/predefined/_sphinx.py
@@ -1,9 +1,13 @@
 from pathlib import Path
-from typing import Callable, List, Optional, Union
+from typing import List, Optional, Union
 
-from .. import Step, StepHandler
-from .. import parametrize as apply_parameters
-from .. import register_managed_step, set_defaults
+from .. import (
+    Step,
+    StepHandler,
+    build_parameters,
+    register_managed_step,
+    set_defaults,
+)
 
 
 @set_defaults(
@@ -62,12 +66,11 @@ def sphinx(
     sourcedir: Optional[Union[Path, str]] = None,
     output: Optional[Union[Path, str]] = None,
     warning_as_error: Optional[bool] = None,
-    parametrize: Optional[Callable[[Step], Step]] = None,
     python: Optional[str] = None,
     requires: Optional[List[str]] = None,
     dependencies: Optional[List[str]] = None,
     run_by_default: Optional[bool] = None,
-) -> None:
+) -> Step:
     """
     Run `sphinx`_.
 
@@ -82,8 +85,6 @@ def sphinx(
                    Defaults to :python:`None`.
     :param warning_as_error: Turn warnings into errors
                              Defaults to :python:`False`.
-    :param parametrize: A :py:func:`dwas.parametrize` invocation to apply on the
-                        step.
     :param python: The version of python to use.
                    Defaults to the version *dwas* was installed with.
     :param requires: A list of other steps that this step would require.
@@ -91,6 +92,7 @@ def sphinx(
                          Defaults to :python:`["pytest"]`.
     :param run_by_default: Whether to run this step by default or not.
                            If :python:`None`, will default to :python:`True`.
+    :return: The step so that you can add additional parameters to it if needed.
 
     :Examples:
 
@@ -110,35 +112,27 @@ def sphinx(
 
         .. code-block::
 
-            dwas.predefined.sphinx(
-                requires=["package"],
-                parametrize=dwas.parametrize(
-                    ("builder", "output"),
-                    [
-                        ("html", "_build/docs"),
-                        # We don't care about the output of those two here.
-                        ("linkcheck", None),
-                        ("doctests", None),
-                    ],
-                    ids=["html", "linkcheck", "doctests"],
-                )
-            )
+            dwas.parametrize(
+                ("builder", "output"),
+                [
+                    ("html", "_build/docs"),
+                    # We don't care about the output of those two here.
+                    ("linkcheck", None),
+                    ("doctests", None),
+                ],
+                ids=["html", "linkcheck", "doctests"],
+            )(dwas.predefined.sphinx(requires=["package"]))
     """
     sphinx_ = Sphinx()
 
-    for parameter, value in {
-        "builder": builder,
-        "sourcedir": sourcedir,
-        "output": output,
-        "warning_as_error": warning_as_error,
-    }.items():
-        if value is not None:
-            sphinx_ = apply_parameters(parameter, [value])(sphinx_)
+    sphinx_ = build_parameters(
+        builder=builder,
+        sourcedir=sourcedir,
+        output=output,
+        warning_as_error=warning_as_error,
+    )(sphinx_)
 
-    if parametrize is not None:
-        sphinx_ = parametrize(sphinx_)  # type: ignore
-
-    register_managed_step(
+    return register_managed_step(
         sphinx_,
         dependencies,
         name=name,