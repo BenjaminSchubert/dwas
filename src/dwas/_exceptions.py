BenjaminSchubert/wast

Add documentation and publish it on readthedocs

src/wast/_exceptions.py
@@ -2,6 +2,18 @@ from typing import List
 
 
 class BaseWastException(Exception):
+    """
+    Base exception used for exceptions thrown by Wast.
+
+    :param message: A user-facing message explaining what happened.
+    :param exit_code:
+
+        the exit code to use for the wast process if the exception is not caught.
+
+        - 1 means a pipeline run failed
+        - 2 means a user or configuration error
+    """
+
     def __init__(self, message: str, exit_code: int = 2) -> None:
         super().__init__(message)
         self._message = message

src/wast/predefined/_black.py
@@ -19,7 +19,8 @@ from .. import (
     }
 )
 class Black(Step):
-    __name__ = "black"
+    def __init__(self) -> None:
+        self.__name__ = "black"
 
     def __call__(
         self,
@@ -47,6 +48,69 @@ def black(
     dependencies: Optional[List[str]] = None,
     run_by_default: Optional[bool] = None,
 ) -> None:
+    """
+    Run `the Black formatter`_ against your python source code.
+
+    :param name: The name to give to the step.
+                 Defaults to :python:`"black"`.
+    :param files: The list of files or directories to run ``black`` against.
+                  Defaults to :python:`["."]`.
+    :param additional_arguments: Additional arguments to pass to the ``black``
+                                 invocation.
+                                 Defaults to :python:`["--check", "--diff", "-W1"]`.
+    :param python: The version of python to use.
+                   Defaults to the version *wast* was installed with.
+    :param requires: A list of other steps that this step would require.
+    :param dependencies: Python dependencies needed to run this step.
+                         Defaults to :python:`["black"]`.
+    :param run_by_default: Whether to run this step by default or not.
+                           Defaults to :python:`True`.
+
+    :Examples:
+
+        In order to verify your code but not change it, for a step
+        named **black**:
+
+        .. code-block::
+
+            wast.predefined.black()
+
+        Or, in order to automatically fix your code, but only if requested:
+
+        .. code-block::
+
+            wast.predefined.black(
+                # Note: this name is arbitrary, you could omit it, or specify
+                #       something else. We suffix in our documentation all
+                #       operations that will have destructive effect on the source
+                #       code by ``:fix``
+                name="black:fix",
+                additional_arguments=[],
+                run_by_default=False,
+            )
+
+        Note that if you have other steps that will overwrite some of your files,
+        you might want to order them so they don't conflict. For example, assuming
+        that you also use :py:func:`wast.predefined.isort`:
+
+        .. code-block::
+
+            wast.predefined.isort(
+                name="isort:fix",
+                additional_arguments=["--atomic"],
+                run_by_default=False,
+            )
+            wast.predefined.black(
+                name="black:fix",
+                requires=["isort:fix"],
+                additional_arguments=[],
+                run_by_default=False,
+            )
+
+        This will ensure that both steps don't step on each other and make black run
+        second.
+    """
+
     black_ = Black()
 
     if files is not None:

src/wast/predefined/_isort.py
@@ -19,7 +19,8 @@ from .. import (
     }
 )
 class Isort(Step):
-    __name__ = "isort"
+    def __init__(self) -> None:
+        self.__name__ = "isort"
 
     def __call__(
         self,
@@ -43,6 +44,53 @@ def isort(
     run_by_default: Optional[bool] = None,
     dependencies: Optional[List[str]] = None,
 ) -> None:
+    """
+    Run `the isort formatter`_ against your python source code.
+
+    :param name: The name to give to the step.
+                 Defaults to :python:`"isort"`.
+    :param files: The list of files or directories to run ``isort`` against.
+                  Defaults to :python:`["."]`.
+    :param additional_arguments: Additional arguments to pass to the ``isort``
+                                 invocation.
+                                 Defaults to :python:`["--check-only", "--diff"]`.
+    :param python: The version of python to use.
+                   Defaults to the version *wast* was installed with.
+    :param requires: A list of other steps that this step would require.
+    :param dependencies: Python dependencies needed to run this step.
+                         Defaults to :python:`["isort[colors]"]`.
+    :param run_by_default: Whether to run this step by default or not.
+                           Defaults to :python:`True`.
+
+    .. tip::
+
+        isort can be quite slow to find all files it needs to handle, you might
+        want to limit the number of directories searched.
+
+    :Examples:
+
+        In order to verify your code but not change it, for a step
+        named **isort**:
+
+        .. code-block::
+
+            wast.predefined.isort(files["src", "tests", "wastfile.py", "setup.py"])
+
+        Or, in order to automatically fix your code, but only if requested:
+
+        .. code-block::
+
+            wast.predefined.isort(
+                # Note: this name is arbitrary, you could omit it, or specify
+                #       something else. We suffix in our documentation all
+                #       operations that will have destructive effect on the source
+                #       code by ``:fix``
+                name="isort:fix",
+                additional_arguments=["--atomic"],
+                run_by_default=False,
+                files=["src,", "tests", "wastfile.py", "setup.py"],
+            )
+    """
     isort_ = Isort()
 
     if files is not None:

src/wast/predefined/_mypy.py
@@ -19,7 +19,8 @@ LOGGER = logging.getLogger(__name__)
     {"dependencies": ["mypy"], "additional_arguments": [], "files": ["."]}
 )
 class Mypy(Step):
-    __name__ = "mypy"
+    def __init__(self) -> None:
+        self.__name__ = "mypy"
 
     def __call__(
         self,
@@ -53,6 +54,36 @@ def mypy(
     dependencies: Optional[Sequence[str]] = None,
     run_by_default: Optional[bool] = None,
 ) -> None:
+    """
+    Run `mypy`_ against your python source code.
+
+    :param name: The name to give to the step.
+                 Defaults to :python:`"mypy"`.
+    :param files: The list of files, directories or packages to run ``mypy``
+                  against.
+                  Defaults to :python:`["."]`.
+    :param additional_arguments: Additional arguments to pass to the ``mypy``
+                                 invocation.
+                                 Defaults to :python:`[]`.
+    :param python: The version of python to use.
+                   Defaults to the version *wast* was installed with.
+    :param requires: A list of other steps that this step would require.
+    :param dependencies: Python dependencies needed to run this step.
+                         Defaults to :python:`["mypy"]`.
+    :param run_by_default: Whether to run this step by default or not.
+                           Defaults to :python:`True`.
+
+    :Examples:
+
+        .. code-block::
+
+            wast.predefined.mypy(
+                # Only run for sources, not tests/ or setup.py
+                files=["./src"],
+                dependencies=["mypy", "types-requests"],
+            )
+
+    """
     mypy_ = Mypy()
 
     if files is not None:

src/wast/predefined/_pytest.py
@@ -14,7 +14,8 @@ LOGGER = logging.getLogger(__name__)
 
 @set_defaults({"dependencies": ["pytest"], "args": []})
 class Pytest(Step):
-    __name__ = "pytest"
+    def __init__(self) -> None:
+        self.__name__ = "pytest"
 
     def gather_artifacts(self, step: StepHandler) -> Dict[str, List[Any]]:
         coverage_file = self._get_coverage_file(step)
@@ -37,12 +38,87 @@ def pytest(
     *,
     name: Optional[str] = None,
     args: Optional[Sequence[str]] = None,
+    parametrize: Optional[Callable[[Step], Step]] = None,
     python: Optional[str] = None,
     requires: Optional[List[str]] = None,
-    run_by_default: Optional[bool] = None,
     dependencies: Optional[Sequence[str]] = None,
-    parametrize: Optional[Callable[[T], T]] = None,
+    run_by_default: Optional[bool] = None,
 ) -> None:
+    """
+    Run `pytest`_.
+
+    :param name: The name to give to the step.
+                 Defaults to :python:`"pytest"`.
+    :param args: arguments to pass to the ``pytest`` invocation.
+                 Defaults to :python:`[]`.
+    :param parametrize: A :py:func:`wast.parametrize` invocation to apply on
+                        the step.
+    :param python: The version of python to use.
+                   Defaults to the version *wast* was installed with.
+    :param requires: A list of other steps that this step would require.
+    :param dependencies: Python dependencies needed to run this step.
+                         Defaults to :python:`["pytest"]`.
+    :param run_by_default: Whether to run this step by default or not.
+                           If :python:`None`, will default to :python:`True`.
+
+    .. seealso::
+
+        How to parametrize methods with :py:func:`wast.parametrize`
+
+    .. tip::
+
+        If you use ``pytest-cov``, it will also automatically expose a
+        ``coverage_files`` :term:`artifact` that can be used by dependent steps,
+        for an example, see :py:func:`coverage`
+
+    :Examples:
+
+        For running pytest with the a specific version of python, with your
+        code in the `PYTHONPATH`:
+
+        .. code-block::
+
+            wast.predefined.pytest(python="3.9")
+
+        Or, for a more concrete example, across multiple versions of python and
+        testing your installed application:
+
+        .. code-block::
+
+            # Setup a "package" step, to install the source code automatically
+            # for the tests
+            wast.predefined.package()
+
+            wast.predefined.pytest(
+                dependencies=["pytest", "pytest-cov"],
+                requires=["package"],
+                parametrize=wast.parametrize("python", ["3.8", "3.9", "3.10"])
+            )
+
+        Leveraging :py:func:`wast.parametrize`, this generates 4 different
+        steps: ``pytest``, which is a :term:`step group` which depends on the
+        other ``pytest[3.8]``, ``pytest[3.9]`` and ``pytest[3.10]`` which each
+        run pytest with the given version of python.
+
+        Similarly, if you wanted to test multiple multiple versions of a python
+        package, with different versions of python:
+
+        .. code-block::
+
+            wast.predefined.pytest(
+                requires=["package"],
+                parametrize=wast.parametrize(
+                    "python", ["3.8", "3.9", "3.10"]
+                )(wast.parametrize(
+                    "dependencies",
+                    [
+                        ["pytest", "django==3.0"],
+                        ["pytest", "django==4.0"],
+                    ],
+                    ids=["django3", "django4"],
+                )
+            )
+    """
     pytest_ = Pytest()
 
     if args is not None:

BenjaminSchubert/dwas

src/dwas/_exceptions.py
@@ -2,6 +2,18 @@ from typing import List
 
 
 class BaseDwasException(Exception):
+    """
+    Base exception used for exceptions thrown by DWAS.
+
+    :param message: A user-facing message explaining what happened.
+    :param exit_code:
+
+        the exit code to use for the dwas process if the exception is not caught.
+
+        - 1 means a pipeline run failed
+        - 2 means a user or configuration error
+    """
+
     def __init__(self, message: str, exit_code: int = 2) -> None:
         super().__init__(message)
         self._message = message

src/dwas/predefined/_black.py
@@ -19,7 +19,8 @@ from .. import (
     }
 )
 class Black(Step):
-    __name__ = "black"
+    def __init__(self) -> None:
+        self.__name__ = "black"
 
     def __call__(
         self,
@@ -47,6 +48,69 @@ def black(
     dependencies: Optional[List[str]] = None,
     run_by_default: Optional[bool] = None,
 ) -> None:
+    """
+    Run `the Black formatter`_ against your python source code.
+
+    :param name: The name to give to the step.
+                 Defaults to :python:`"black"`.
+    :param files: The list of files or directories to run ``black`` against.
+                  Defaults to :python:`["."]`.
+    :param additional_arguments: Additional arguments to pass to the ``black``
+                                 invocation.
+                                 Defaults to :python:`["--check", "--diff", "-W1"]`.
+    :param python: The version of python to use.
+                   Defaults to the version *dwas* was installed with.
+    :param requires: A list of other steps that this step would require.
+    :param dependencies: Python dependencies needed to run this step.
+                         Defaults to :python:`["black"]`.
+    :param run_by_default: Whether to run this step by default or not.
+                           Defaults to :python:`True`.
+
+    :Examples:
+
+        In order to verify your code but not change it, for a step
+        named **black**:
+
+        .. code-block::
+
+            dwas.predefined.black()
+
+        Or, in order to automatically fix your code, but only if requested:
+
+        .. code-block::
+
+            dwas.predefined.black(
+                # Note: this name is arbitrary, you could omit it, or specify
+                #       something else. We suffix in our documentation all
+                #       operations that will have destructive effect on the source
+                #       code by ``:fix``
+                name="black:fix",
+                additional_arguments=[],
+                run_by_default=False,
+            )
+
+        Note that if you have other steps that will overwrite some of your files,
+        you might want to order them so they don't conflict. For example, assuming
+        that you also use :py:func:`dwas.predefined.isort`:
+
+        .. code-block::
+
+            dwas.predefined.isort(
+                name="isort:fix",
+                additional_arguments=["--atomic"],
+                run_by_default=False,
+            )
+            dwas.predefined.black(
+                name="black:fix",
+                requires=["isort:fix"],
+                additional_arguments=[],
+                run_by_default=False,
+            )
+
+        This will ensure that both steps don't step on each other and make black run
+        second.
+    """
+
     black_ = Black()
 
     if files is not None:

src/dwas/predefined/_isort.py
@@ -19,7 +19,8 @@ from .. import (
     }
 )
 class Isort(Step):
-    __name__ = "isort"
+    def __init__(self) -> None:
+        self.__name__ = "isort"
 
     def __call__(
         self,
@@ -43,6 +44,53 @@ def isort(
     run_by_default: Optional[bool] = None,
     dependencies: Optional[List[str]] = None,
 ) -> None:
+    """
+    Run `the isort formatter`_ against your python source code.
+
+    :param name: The name to give to the step.
+                 Defaults to :python:`"isort"`.
+    :param files: The list of files or directories to run ``isort`` against.
+                  Defaults to :python:`["."]`.
+    :param additional_arguments: Additional arguments to pass to the ``isort``
+                                 invocation.
+                                 Defaults to :python:`["--check-only", "--diff"]`.
+    :param python: The version of python to use.
+                   Defaults to the version *dwas* was installed with.
+    :param requires: A list of other steps that this step would require.
+    :param dependencies: Python dependencies needed to run this step.
+                         Defaults to :python:`["isort[colors]"]`.
+    :param run_by_default: Whether to run this step by default or not.
+                           Defaults to :python:`True`.
+
+    .. tip::
+
+        isort can be quite slow to find all files it needs to handle, you might
+        want to limit the number of directories searched.
+
+    :Examples:
+
+        In order to verify your code but not change it, for a step
+        named **isort**:
+
+        .. code-block::
+
+            dwas.predefined.isort(files["src", "tests", "dwasfile.py", "setup.py"])
+
+        Or, in order to automatically fix your code, but only if requested:
+
+        .. code-block::
+
+            dwas.predefined.isort(
+                # Note: this name is arbitrary, you could omit it, or specify
+                #       something else. We suffix in our documentation all
+                #       operations that will have destructive effect on the source
+                #       code by ``:fix``
+                name="isort:fix",
+                additional_arguments=["--atomic"],
+                run_by_default=False,
+                files=["src,", "tests", "dwasfile.py", "setup.py"],
+            )
+    """
     isort_ = Isort()
 
     if files is not None:

src/dwas/predefined/_mypy.py
@@ -19,7 +19,8 @@ LOGGER = logging.getLogger(__name__)
     {"dependencies": ["mypy"], "additional_arguments": [], "files": ["."]}
 )
 class Mypy(Step):
-    __name__ = "mypy"
+    def __init__(self) -> None:
+        self.__name__ = "mypy"
 
     def __call__(
         self,
@@ -53,6 +54,36 @@ def mypy(
     dependencies: Optional[Sequence[str]] = None,
     run_by_default: Optional[bool] = None,
 ) -> None:
+    """
+    Run `mypy`_ against your python source code.
+
+    :param name: The name to give to the step.
+                 Defaults to :python:`"mypy"`.
+    :param files: The list of files, directories or packages to run ``mypy``
+                  against.
+                  Defaults to :python:`["."]`.
+    :param additional_arguments: Additional arguments to pass to the ``mypy``
+                                 invocation.
+                                 Defaults to :python:`[]`.
+    :param python: The version of python to use.
+                   Defaults to the version *dwas* was installed with.
+    :param requires: A list of other steps that this step would require.
+    :param dependencies: Python dependencies needed to run this step.
+                         Defaults to :python:`["mypy"]`.
+    :param run_by_default: Whether to run this step by default or not.
+                           Defaults to :python:`True`.
+
+    :Examples:
+
+        .. code-block::
+
+            dwas.predefined.mypy(
+                # Only run for sources, not tests/ or setup.py
+                files=["./src"],
+                dependencies=["mypy", "types-requests"],
+            )
+
+    """
     mypy_ = Mypy()
 
     if files is not None:

src/dwas/predefined/_pytest.py
@@ -14,7 +14,8 @@ LOGGER = logging.getLogger(__name__)
 
 @set_defaults({"dependencies": ["pytest"], "args": []})
 class Pytest(Step):
-    __name__ = "pytest"
+    def __init__(self) -> None:
+        self.__name__ = "pytest"
 
     def gather_artifacts(self, step: StepHandler) -> Dict[str, List[Any]]:
         coverage_file = self._get_coverage_file(step)
@@ -37,12 +38,87 @@ def pytest(
     *,
     name: Optional[str] = None,
     args: Optional[Sequence[str]] = None,
+    parametrize: Optional[Callable[[Step], Step]] = None,
     python: Optional[str] = None,
     requires: Optional[List[str]] = None,
-    run_by_default: Optional[bool] = None,
     dependencies: Optional[Sequence[str]] = None,
-    parametrize: Optional[Callable[[T], T]] = None,
+    run_by_default: Optional[bool] = None,
 ) -> None:
+    """
+    Run `pytest`_.
+
+    :param name: The name to give to the step.
+                 Defaults to :python:`"pytest"`.
+    :param args: arguments to pass to the ``pytest`` invocation.
+                 Defaults to :python:`[]`.
+    :param parametrize: A :py:func:`dwas.parametrize` invocation to apply on
+                        the step.
+    :param python: The version of python to use.
+                   Defaults to the version *dwas* was installed with.
+    :param requires: A list of other steps that this step would require.
+    :param dependencies: Python dependencies needed to run this step.
+                         Defaults to :python:`["pytest"]`.
+    :param run_by_default: Whether to run this step by default or not.
+                           If :python:`None`, will default to :python:`True`.
+
+    .. seealso::
+
+        How to parametrize methods with :py:func:`dwas.parametrize`
+
+    .. tip::
+
+        If you use ``pytest-cov``, it will also automatically expose a
+        ``coverage_files`` :term:`artifact` that can be used by dependent steps,
+        for an example, see :py:func:`coverage`
+
+    :Examples:
+
+        For running pytest with the a specific version of python, with your
+        code in the `PYTHONPATH`:
+
+        .. code-block::
+
+            dwas.predefined.pytest(python="3.9")
+
+        Or, for a more concrete example, across multiple versions of python and
+        testing your installed application:
+
+        .. code-block::
+
+            # Setup a "package" step, to install the source code automatically
+            # for the tests
+            dwas.predefined.package()
+
+            dwas.predefined.pytest(
+                dependencies=["pytest", "pytest-cov"],
+                requires=["package"],
+                parametrize=dwas.parametrize("python", ["3.8", "3.9", "3.10"])
+            )
+
+        Leveraging :py:func:`dwas.parametrize`, this generates 4 different
+        steps: ``pytest``, which is a :term:`step group` which depends on the
+        other ``pytest[3.8]``, ``pytest[3.9]`` and ``pytest[3.10]`` which each
+        run pytest with the given version of python.
+
+        Similarly, if you wanted to test multiple multiple versions of a python
+        package, with different versions of python:
+
+        .. code-block::
+
+            dwas.predefined.pytest(
+                requires=["package"],
+                parametrize=dwas.parametrize(
+                    "python", ["3.8", "3.9", "3.10"]
+                )(dwas.parametrize(
+                    "dependencies",
+                    [
+                        ["pytest", "django==3.0"],
+                        ["pytest", "django==4.0"],
+                    ],
+                    ids=["django3", "django4"],
+                )
+            )
+    """
     pytest_ = Pytest()
 
     if args is not None: