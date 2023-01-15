BenjaminSchubert/wast

Refactor how predefined steps are exposed to limit the API breakages
This makes it easier to expand the `step` helpers, as they are now not
tied to the predefined steps anymore, and it helps with keeping concerns
separate

src/wast/predefined/_coverage.py
@@ -2,13 +2,7 @@ from typing import List, Optional
 
 # XXX: All imports here should be done from the top level. If we need it,
 #      users might need it
-from .. import (
-    Step,
-    StepRunner,
-    parametrize,
-    register_managed_step,
-    set_defaults,
-)
+from .. import Step, StepRunner, parametrize, set_defaults
 
 
 @set_defaults(
@@ -40,30 +34,16 @@ class Coverage(Step):
             step.run(["coverage", *report], env=env)
 
 
-def coverage(
-    *,
-    name: Optional[str] = None,
-    reports: Optional[List[List[str]]] = None,
-    python: Optional[str] = None,
-    requires: Optional[List[str]] = None,
-    dependencies: Optional[List[str]] = None,
-    run_by_default: Optional[bool] = None,
-) -> Step:
+def coverage(*, reports: Optional[List[List[str]]] = None) -> Step:
     """
     Run `coverage.py`_ to generate coverage reports.
 
-    :param name: The name to give to the step.
-                 Defaults to :python:`"coverage"`.
+    By default, it will depend on :python:`["coverage"]`, when registered with
+    :py:func:`wast.register_managed_step`.
+
     :param reports: A list of parameters to pass to coverage to generate
                     reports.
                     Defaults to :python:`[["report", "--show-missing"]]`
-    :param python: The version of python to use.
-                   Defaults to the version *wast* was installed with.
-    :param requires: A list of other steps that this step would require.
-    :param dependencies: Python dependencies needed to run this step.
-                         Defaults to :python:`["coverage"]`.
-    :param run_by_default: Whether to run this step by default or not.
-                           Defaults to :python:`True`.
 
     This step leverages :term:`artifacts<artifact>` named ``coverage_files`` provided by
     other steps to provide reports.
@@ -76,22 +56,26 @@ def coverage(
         .. code-block::
 
             # One step to generate the package
-            wast.predefined.package()
+            wast.register_managed_step(wast.predefined.package())
 
             # One step to run pytest across multiple python versions
-            wast.predefined.pytest(
-                dependencies=["pytest", "pytest-cov"],
-                requires=["package"],
-                parametrize=wast.parametrize("python", ["3.8", "3.9", "3.10"])
+            wast.register_managed_step(
+                wast.parametrize("python", ["3.8", "3.9", "3.10"])(
+                    wast.predefined.pytest()
+                ),
+                dependencies=["pytest', "pytest-cov"],
+                requires=["package"]
             )
 
             # And finally generate xml report and one on stdout.
             # This will combine all coverage info from the previous pytest runs.
-            wast.predefined.coverage(
-                reports=[
-                    ["xml", "-o", "reports/coverage.xml"],
-                    ["report", "--show-missing"],
-                ],
+            wast.register_managed_step(
+                wast.predefined.coverage(
+                    reports=[
+                        ["xml", "-o", "reports/coverage.xml"],
+                        ["report", "--show-missing"],
+                    ],
+                ),
                 requires=["pytest"],
             )
 
@@ -102,11 +86,4 @@ def coverage(
     if reports is not None:
         coverage_ = parametrize("reports", [reports])(coverage_)
 
-    return register_managed_step(
-        coverage_,
-        dependencies,
-        name=name,
-        python=python,
-        requires=requires,
-        run_by_default=run_by_default,
-    )
+    return coverage_

src/wast/predefined/_mypy.py
@@ -4,13 +4,7 @@ from typing import List, Optional, Sequence
 
 # XXX: All imports here should be done from the top level. If we need it,
 #      users might need it
-from .. import (
-    Step,
-    StepRunner,
-    build_parameters,
-    register_managed_step,
-    set_defaults,
-)
+from .. import Step, StepRunner, build_parameters, set_defaults
 
 LOGGER = logging.getLogger(__name__)
 
@@ -46,56 +40,34 @@ class Mypy(Step):
 
 def mypy(
     *,
-    name: Optional[str] = None,
     files: Optional[Sequence[str]] = None,
     additional_arguments: Optional[List[str]] = None,
-    python: Optional[str] = None,
-    requires: Optional[List[str]] = None,
-    dependencies: Optional[Sequence[str]] = None,
-    run_by_default: Optional[bool] = None,
 ) -> Step:
     """
     Run `mypy`_ against your python source code.
 
-    :param name: The name to give to the step.
-                 Defaults to :python:`"mypy"`.
+    By default, it will depend on :python:`["mypy"]`, when registered with
+    :py:func:`wast.register_managed_step`.
+
     :param files: The list of files, directories or packages to run ``mypy``
                   against.
                   Defaults to :python:`["."]`.
     :param additional_arguments: Additional arguments to pass to the ``mypy``
                                  invocation.
                                  Defaults to :python:`[]`.
-    :param python: The version of python to use.
-                   Defaults to the version *wast* was installed with.
-    :param requires: A list of other steps that this step would require.
-    :param dependencies: Python dependencies needed to run this step.
-                         Defaults to :python:`["mypy"]`.
-    :param run_by_default: Whether to run this step by default or not.
-                           Defaults to :python:`True`.
     :return: The step so that you can add additional parameters to it if needed.
 
     :Examples:
 
         .. code-block::
 
-            wast.predefined.mypy(
+            wast.register_managed_step(
                 # Only run for sources, not tests/ or setup.py
-                files=["./src"],
+                wast.predefined.mypy(files=["./src"]),
                 dependencies=["mypy", "types-requests"],
             )
     """
-    mypy_ = Mypy()
-
-    mypy_ = build_parameters(
+    return build_parameters(
         files=files,
         additional_arguments=additional_arguments,
-    )(mypy_)
-
-    return register_managed_step(
-        mypy_,
-        dependencies,
-        name=name,
-        python=python,
-        requires=requires,
-        run_by_default=run_by_default,
-    )
+    )(Mypy())

src/wast/predefined/_pylint.py
@@ -2,13 +2,7 @@ from typing import List, Optional, Sequence
 
 # XXX: All imports here should be done from the top level. If we need it,
 #      users might need it
-from .. import (
-    Step,
-    StepRunner,
-    build_parameters,
-    register_managed_step,
-    set_defaults,
-)
+from .. import Step, StepRunner, build_parameters, set_defaults
 
 
 @set_defaults(
@@ -41,56 +35,34 @@ class Pylint(Step):
 
 def pylint(
     *,
-    name: str = "pylint",
     files: Optional[Sequence[str]] = None,
     additional_arguments: Optional[Sequence[str]] = None,
-    python: Optional[str] = None,
-    requires: Optional[List[str]] = None,
-    dependencies: Optional[Sequence[str]] = None,
-    run_by_default: Optional[bool] = None,
 ) -> Step:
     """
     Run `pylint`_ against your source code.
 
-    :param name: The name to give to the step.
-                 Defaults to :python:`"pylint"`.
+    By default, it will depend on :python:`["pylint"]`, when registered with
+    :py:func:`wast.register_managed_step`.
+
     :param files: The list of files or directories to run ``pylint`` against.
                   Defaults to :python:`["."]`.
     :param additional_arguments: Additional arguments to pass to the ``pylint``
                                  invocation.
                                  Defaults to :python:`[]`.
-    :param python: The version of python to use.
-                   Defaults to the version *wast* was installed with.
-    :param requires: A list of other steps that this step would require.
-    :param dependencies: Python dependencies needed to run this step.
-                         Defaults to :python:`["pylint"]`.
-    :param run_by_default: Whether to run this step by default or not.
-                           Defaults to :python:`True`.
     :return: The step so that you can add additional parameters to it if needed.
 
     :Examples:
 
         .. code-block::
 
-            wast.predefined.pylint(
-                files=["./src", "./tests"],
+            wast.register_managed_step(
+                wast.predefined.pylint(files=["./src", "./tests"]),
                 # Install both test and package dependencies to make pylint
                 # find them
                 dependencies=["requests", "pytest", "pylint"],
             )
     """
-    pylint_ = Pylint()
-
-    pylint_ = build_parameters(
+    return build_parameters(
         files=files,
         additional_arguments=additional_arguments,
-    )(pylint_)
-
-    return register_managed_step(
-        pylint_,
-        dependencies,
-        name=name,
-        python=python,
-        requires=requires,
-        run_by_default=run_by_default,
-    )
+    )(Pylint())

src/wast/predefined/_sphinx.py
@@ -1,15 +1,9 @@
 import shutil
 from contextlib import suppress
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import Optional, Union
 
-from .. import (
-    Step,
-    StepRunner,
-    build_parameters,
-    register_managed_step,
-    set_defaults,
-)
+from .. import Step, StepRunner, build_parameters, set_defaults
 
 
 @set_defaults(
@@ -68,21 +62,17 @@ class Sphinx(Step):
 
 def sphinx(
     *,
-    name: Optional[str] = None,
     builder: Optional[str] = None,
     sourcedir: Optional[Union[Path, str]] = None,
     output: Optional[Union[Path, str]] = None,
     warning_as_error: Optional[bool] = None,
-    python: Optional[str] = None,
-    requires: Optional[List[str]] = None,
-    dependencies: Optional[List[str]] = None,
-    run_by_default: Optional[bool] = None,
 ) -> Step:
     """
     Run `sphinx`_.
 
-    :param name: The name to give to the step.
-                 Defaults to :python:`"sphinx"`.
+    By default, it will depend on :python:`["sphinx"]`, when registered with
+    :py:func:`wast.register_managed_step`.
+
     :param builder: The sphinx builder to use.
                     Defaults to :python:`"html"`.
     :param sourcedir: The directory in which the ``conf.py`` resides.
@@ -92,13 +82,6 @@ def sphinx(
                    Defaults to :python:`None`.
     :param warning_as_error: Turn warnings into errors
                              Defaults to :python:`False`.
-    :param python: The version of python to use.
-                   Defaults to the version *wast* was installed with.
-    :param requires: A list of other steps that this step would require.
-    :param dependencies: Python dependencies needed to run this step.
-                         Defaults to :python:`["pytest"]`.
-    :param run_by_default: Whether to run this step by default or not.
-                           If :python:`None`, will default to :python:`True`.
     :return: The step so that you can add additional parameters to it if needed.
 
     :Examples:
@@ -109,8 +92,9 @@ def sphinx(
 
         .. code-block::
 
-            wast.predefined.sphinx(
-                sourcedir="docs", output="_build/docs", python="3.8"
+            wast.register_managed_step(
+                wast.predefined.sphinx(sourcedir="docs", output="_build/docs"),
+                python="3.8"
             )
 
         Or, to run doctests, linkchecks and build the output to ``_build/docs``,
@@ -119,31 +103,23 @@ def sphinx(
 
         .. code-block::
 
-            wast.parametrize(
-                ("builder", "output"),
-                [
-                    ("html", "_build/docs"),
-                    # We don't care about the output of those two here.
-                    ("linkcheck", None),
-                    ("doctests", None),
-                ],
-                ids=["html", "linkcheck", "doctests"],
-            )(wast.predefined.sphinx(requires=["package"]))
+            register_managed_step(
+                wast.parametrize(
+                    ("builder", "output"),
+                    [
+                        ("html", "_build/docs"),
+                        # We don't care about the output of those two here.
+                        ("linkcheck", None),
+                        ("doctests", None),
+                    ],
+                    ids=["html", "linkcheck", "doctests"],
+                )(wast.predefined.sphinx()),
+                requires=["package"],
+            )
     """
-    sphinx_ = Sphinx()
-
-    sphinx_ = build_parameters(
+    return build_parameters(
         builder=builder,
         sourcedir=sourcedir,
         output=output,
         warning_as_error=warning_as_error,
-    )(sphinx_)
-
-    return register_managed_step(
-        sphinx_,
-        dependencies,
-        name=name,
-        python=python,
-        requires=requires,
-        run_by_default=run_by_default,
-    )
+    )(Sphinx())

tests/predefined/test_unimport.py
@@ -1,10 +1,11 @@
 def test_does_not_modify_files_by_default(cli, tmp_path):
     # This code is misformatted to trigger an error
     wastfile_content = """\
+from wast import register_managed_step
 from wast.predefined import unimport
 import os
 
-unimport()
+register_managed_step(unimport())
 """
     wastfile_path = tmp_path.joinpath("wastfile.py")
     wastfile_path.write_text(wastfile_content)
@@ -18,10 +19,13 @@ unimport()
 def test_can_apply_fixes(cli, tmp_path):
     # This code is misformatted to trigger an error
     wastfile_content = """\
+from wast import register_managed_step
 from wast.predefined import unimport
 import os
 
-unimport(additional_arguments=["--diff", "--remove", "--check"])
+register_managed_step(
+    unimport(additional_arguments=["--diff", "--remove", "--check"])
+)
 """
     wastfile_path = tmp_path.joinpath("wastfile.py")
     wastfile_path.write_text(wastfile_content)

BenjaminSchubert/dwas

src/dwas/predefined/_coverage.py
@@ -2,13 +2,7 @@ from typing import List, Optional
 
 # XXX: All imports here should be done from the top level. If we need it,
 #      users might need it
-from .. import (
-    Step,
-    StepRunner,
-    parametrize,
-    register_managed_step,
-    set_defaults,
-)
+from .. import Step, StepRunner, parametrize, set_defaults
 
 
 @set_defaults(
@@ -40,30 +34,16 @@ class Coverage(Step):
             step.run(["coverage", *report], env=env)
 
 
-def coverage(
-    *,
-    name: Optional[str] = None,
-    reports: Optional[List[List[str]]] = None,
-    python: Optional[str] = None,
-    requires: Optional[List[str]] = None,
-    dependencies: Optional[List[str]] = None,
-    run_by_default: Optional[bool] = None,
-) -> Step:
+def coverage(*, reports: Optional[List[List[str]]] = None) -> Step:
     """
     Run `coverage.py`_ to generate coverage reports.
 
-    :param name: The name to give to the step.
-                 Defaults to :python:`"coverage"`.
+    By default, it will depend on :python:`["coverage"]`, when registered with
+    :py:func:`dwas.register_managed_step`.
+
     :param reports: A list of parameters to pass to coverage to generate
                     reports.
                     Defaults to :python:`[["report", "--show-missing"]]`
-    :param python: The version of python to use.
-                   Defaults to the version *dwas* was installed with.
-    :param requires: A list of other steps that this step would require.
-    :param dependencies: Python dependencies needed to run this step.
-                         Defaults to :python:`["coverage"]`.
-    :param run_by_default: Whether to run this step by default or not.
-                           Defaults to :python:`True`.
 
     This step leverages :term:`artifacts<artifact>` named ``coverage_files`` provided by
     other steps to provide reports.
@@ -76,22 +56,26 @@ def coverage(
         .. code-block::
 
             # One step to generate the package
-            dwas.predefined.package()
+            dwas.register_managed_step(dwas.predefined.package())
 
             # One step to run pytest across multiple python versions
-            dwas.predefined.pytest(
-                dependencies=["pytest", "pytest-cov"],
-                requires=["package"],
-                parametrize=dwas.parametrize("python", ["3.8", "3.9", "3.10"])
+            dwas.register_managed_step(
+                dwas.parametrize("python", ["3.8", "3.9", "3.10"])(
+                    dwas.predefined.pytest()
+                ),
+                dependencies=["pytest', "pytest-cov"],
+                requires=["package"]
             )
 
             # And finally generate xml report and one on stdout.
             # This will combine all coverage info from the previous pytest runs.
-            dwas.predefined.coverage(
-                reports=[
-                    ["xml", "-o", "reports/coverage.xml"],
-                    ["report", "--show-missing"],
-                ],
+            dwas.register_managed_step(
+                dwas.predefined.coverage(
+                    reports=[
+                        ["xml", "-o", "reports/coverage.xml"],
+                        ["report", "--show-missing"],
+                    ],
+                ),
                 requires=["pytest"],
             )
 
@@ -102,11 +86,4 @@ def coverage(
     if reports is not None:
         coverage_ = parametrize("reports", [reports])(coverage_)
 
-    return register_managed_step(
-        coverage_,
-        dependencies,
-        name=name,
-        python=python,
-        requires=requires,
-        run_by_default=run_by_default,
-    )
+    return coverage_

src/dwas/predefined/_mypy.py
@@ -4,13 +4,7 @@ from typing import List, Optional, Sequence
 
 # XXX: All imports here should be done from the top level. If we need it,
 #      users might need it
-from .. import (
-    Step,
-    StepRunner,
-    build_parameters,
-    register_managed_step,
-    set_defaults,
-)
+from .. import Step, StepRunner, build_parameters, set_defaults
 
 LOGGER = logging.getLogger(__name__)
 
@@ -46,56 +40,34 @@ class Mypy(Step):
 
 def mypy(
     *,
-    name: Optional[str] = None,
     files: Optional[Sequence[str]] = None,
     additional_arguments: Optional[List[str]] = None,
-    python: Optional[str] = None,
-    requires: Optional[List[str]] = None,
-    dependencies: Optional[Sequence[str]] = None,
-    run_by_default: Optional[bool] = None,
 ) -> Step:
     """
     Run `mypy`_ against your python source code.
 
-    :param name: The name to give to the step.
-                 Defaults to :python:`"mypy"`.
+    By default, it will depend on :python:`["mypy"]`, when registered with
+    :py:func:`dwas.register_managed_step`.
+
     :param files: The list of files, directories or packages to run ``mypy``
                   against.
                   Defaults to :python:`["."]`.
     :param additional_arguments: Additional arguments to pass to the ``mypy``
                                  invocation.
                                  Defaults to :python:`[]`.
-    :param python: The version of python to use.
-                   Defaults to the version *dwas* was installed with.
-    :param requires: A list of other steps that this step would require.
-    :param dependencies: Python dependencies needed to run this step.
-                         Defaults to :python:`["mypy"]`.
-    :param run_by_default: Whether to run this step by default or not.
-                           Defaults to :python:`True`.
     :return: The step so that you can add additional parameters to it if needed.
 
     :Examples:
 
         .. code-block::
 
-            dwas.predefined.mypy(
+            dwas.register_managed_step(
                 # Only run for sources, not tests/ or setup.py
-                files=["./src"],
+                dwas.predefined.mypy(files=["./src"]),
                 dependencies=["mypy", "types-requests"],
             )
     """
-    mypy_ = Mypy()
-
-    mypy_ = build_parameters(
+    return build_parameters(
         files=files,
         additional_arguments=additional_arguments,
-    )(mypy_)
-
-    return register_managed_step(
-        mypy_,
-        dependencies,
-        name=name,
-        python=python,
-        requires=requires,
-        run_by_default=run_by_default,
-    )
+    )(Mypy())

src/dwas/predefined/_pylint.py
@@ -2,13 +2,7 @@ from typing import List, Optional, Sequence
 
 # XXX: All imports here should be done from the top level. If we need it,
 #      users might need it
-from .. import (
-    Step,
-    StepRunner,
-    build_parameters,
-    register_managed_step,
-    set_defaults,
-)
+from .. import Step, StepRunner, build_parameters, set_defaults
 
 
 @set_defaults(
@@ -41,56 +35,34 @@ class Pylint(Step):
 
 def pylint(
     *,
-    name: str = "pylint",
     files: Optional[Sequence[str]] = None,
     additional_arguments: Optional[Sequence[str]] = None,
-    python: Optional[str] = None,
-    requires: Optional[List[str]] = None,
-    dependencies: Optional[Sequence[str]] = None,
-    run_by_default: Optional[bool] = None,
 ) -> Step:
     """
     Run `pylint`_ against your source code.
 
-    :param name: The name to give to the step.
-                 Defaults to :python:`"pylint"`.
+    By default, it will depend on :python:`["pylint"]`, when registered with
+    :py:func:`dwas.register_managed_step`.
+
     :param files: The list of files or directories to run ``pylint`` against.
                   Defaults to :python:`["."]`.
     :param additional_arguments: Additional arguments to pass to the ``pylint``
                                  invocation.
                                  Defaults to :python:`[]`.
-    :param python: The version of python to use.
-                   Defaults to the version *dwas* was installed with.
-    :param requires: A list of other steps that this step would require.
-    :param dependencies: Python dependencies needed to run this step.
-                         Defaults to :python:`["pylint"]`.
-    :param run_by_default: Whether to run this step by default or not.
-                           Defaults to :python:`True`.
     :return: The step so that you can add additional parameters to it if needed.
 
     :Examples:
 
         .. code-block::
 
-            dwas.predefined.pylint(
-                files=["./src", "./tests"],
+            dwas.register_managed_step(
+                dwas.predefined.pylint(files=["./src", "./tests"]),
                 # Install both test and package dependencies to make pylint
                 # find them
                 dependencies=["requests", "pytest", "pylint"],
             )
     """
-    pylint_ = Pylint()
-
-    pylint_ = build_parameters(
+    return build_parameters(
         files=files,
         additional_arguments=additional_arguments,
-    )(pylint_)
-
-    return register_managed_step(
-        pylint_,
-        dependencies,
-        name=name,
-        python=python,
-        requires=requires,
-        run_by_default=run_by_default,
-    )
+    )(Pylint())

src/dwas/predefined/_sphinx.py
@@ -1,15 +1,9 @@
 import shutil
 from contextlib import suppress
 from pathlib import Path
-from typing import List, Optional, Union
+from typing import Optional, Union
 
-from .. import (
-    Step,
-    StepRunner,
-    build_parameters,
-    register_managed_step,
-    set_defaults,
-)
+from .. import Step, StepRunner, build_parameters, set_defaults
 
 
 @set_defaults(
@@ -68,21 +62,17 @@ class Sphinx(Step):
 
 def sphinx(
     *,
-    name: Optional[str] = None,
     builder: Optional[str] = None,
     sourcedir: Optional[Union[Path, str]] = None,
     output: Optional[Union[Path, str]] = None,
     warning_as_error: Optional[bool] = None,
-    python: Optional[str] = None,
-    requires: Optional[List[str]] = None,
-    dependencies: Optional[List[str]] = None,
-    run_by_default: Optional[bool] = None,
 ) -> Step:
     """
     Run `sphinx`_.
 
-    :param name: The name to give to the step.
-                 Defaults to :python:`"sphinx"`.
+    By default, it will depend on :python:`["sphinx"]`, when registered with
+    :py:func:`dwas.register_managed_step`.
+
     :param builder: The sphinx builder to use.
                     Defaults to :python:`"html"`.
     :param sourcedir: The directory in which the ``conf.py`` resides.
@@ -92,13 +82,6 @@ def sphinx(
                    Defaults to :python:`None`.
     :param warning_as_error: Turn warnings into errors
                              Defaults to :python:`False`.
-    :param python: The version of python to use.
-                   Defaults to the version *dwas* was installed with.
-    :param requires: A list of other steps that this step would require.
-    :param dependencies: Python dependencies needed to run this step.
-                         Defaults to :python:`["pytest"]`.
-    :param run_by_default: Whether to run this step by default or not.
-                           If :python:`None`, will default to :python:`True`.
     :return: The step so that you can add additional parameters to it if needed.
 
     :Examples:
@@ -109,8 +92,9 @@ def sphinx(
 
         .. code-block::
 
-            dwas.predefined.sphinx(
-                sourcedir="docs", output="_build/docs", python="3.8"
+            dwas.register_managed_step(
+                dwas.predefined.sphinx(sourcedir="docs", output="_build/docs"),
+                python="3.8"
             )
 
         Or, to run doctests, linkchecks and build the output to ``_build/docs``,
@@ -119,31 +103,23 @@ def sphinx(
 
         .. code-block::
 
-            dwas.parametrize(
-                ("builder", "output"),
-                [
-                    ("html", "_build/docs"),
-                    # We don't care about the output of those two here.
-                    ("linkcheck", None),
-                    ("doctests", None),
-                ],
-                ids=["html", "linkcheck", "doctests"],
-            )(dwas.predefined.sphinx(requires=["package"]))
+            register_managed_step(
+                dwas.parametrize(
+                    ("builder", "output"),
+                    [
+                        ("html", "_build/docs"),
+                        # We don't care about the output of those two here.
+                        ("linkcheck", None),
+                        ("doctests", None),
+                    ],
+                    ids=["html", "linkcheck", "doctests"],
+                )(dwas.predefined.sphinx()),
+                requires=["package"],
+            )
     """
-    sphinx_ = Sphinx()
-
-    sphinx_ = build_parameters(
+    return build_parameters(
         builder=builder,
         sourcedir=sourcedir,
         output=output,
         warning_as_error=warning_as_error,
-    )(sphinx_)
-
-    return register_managed_step(
-        sphinx_,
-        dependencies,
-        name=name,
-        python=python,
-        requires=requires,
-        run_by_default=run_by_default,
-    )
+    )(Sphinx())

tests/predefined/test_unimport.py
@@ -1,10 +1,11 @@
 def test_does_not_modify_files_by_default(cli, tmp_path):
     # This code is misformatted to trigger an error
     dwasfile_content = """\
+from dwas import register_managed_step
 from dwas.predefined import unimport
 import os
 
-unimport()
+register_managed_step(unimport())
 """
     dwasfile_path = tmp_path.joinpath("dwasfile.py")
     dwasfile_path.write_text(dwasfile_content)
@@ -18,10 +19,13 @@ unimport()
 def test_can_apply_fixes(cli, tmp_path):
     # This code is misformatted to trigger an error
     dwasfile_content = """\
+from dwas import register_managed_step
 from dwas.predefined import unimport
 import os
 
-unimport(additional_arguments=["--diff", "--remove", "--check"])
+register_managed_step(
+    unimport(additional_arguments=["--diff", "--remove", "--check"])
+)
 """
     dwasfile_path = tmp_path.joinpath("dwasfile.py")
     dwasfile_path.write_text(dwasfile_content)