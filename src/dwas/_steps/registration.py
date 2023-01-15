BenjaminSchubert/wast

Handle nicely when trying to register a managed step that implements setup()
Previously, that would throw an exception with no information on what
went wrong.

src/wast/_steps/registration.py
@@ -117,8 +117,12 @@ def register_managed_step(
     :return: The step that was passed as argument.
     """
     if hasattr(func, "setup"):
-        # FIXME: handle nicely
-        raise Exception("NOOOO")
+        raise BaseWastException(
+            f"the step at {get_location(func)} already implements `setup`,"
+            " cannot override it to install dependencies."
+            " You can add `step.install(*dependencies)` inside your setup"
+            " function to handle them."
+        )
 
     # pylint: disable=redefined-outer-name
     def install(step: StepHandlerProtocol, dependencies: str) -> None:

tests/test_registration.py
@@ -158,3 +158,20 @@ def test_can_register_managed_step(pipeline, from_parameters):
     assert steps == {
         "noop": _expect_step(parameters={"dependencies": ["one"]})
     }
+
+
+@isolated_context
+def test_cannot_override_setup_with_managed_step(pipeline):
+    set_pipeline(pipeline)
+
+    class Noop:
+        def setup(self) -> None:
+            pass
+
+        def __call__(self) -> None:
+            pass
+
+    with pytest.raises(BaseWastException) as exc_wrapper:
+        register_managed_step(Noop())
+
+    assert "already implements `setup`" in str(exc_wrapper.value)

BenjaminSchubert/dwas

src/dwas/_steps/registration.py
@@ -117,8 +117,12 @@ def register_managed_step(
     :return: The step that was passed as argument.
     """
     if hasattr(func, "setup"):
-        # FIXME: handle nicely
-        raise Exception("NOOOO")
+        raise BaseDwasException(
+            f"the step at {get_location(func)} already implements `setup`,"
+            " cannot override it to install dependencies."
+            " You can add `step.install(*dependencies)` inside your setup"
+            " function to handle them."
+        )
 
     # pylint: disable=redefined-outer-name
     def install(step: StepHandlerProtocol, dependencies: str) -> None:

tests/test_registration.py
@@ -158,3 +158,20 @@ def test_can_register_managed_step(pipeline, from_parameters):
     assert steps == {
         "noop": _expect_step(parameters={"dependencies": ["one"]})
     }
+
+
+@isolated_context
+def test_cannot_override_setup_with_managed_step(pipeline):
+    set_pipeline(pipeline)
+
+    class Noop:
+        def setup(self) -> None:
+            pass
+
+        def __call__(self) -> None:
+            pass
+
+    with pytest.raises(BaseDwasException) as exc_wrapper:
+        register_managed_step(Noop())
+
+    assert "already implements `setup`" in str(exc_wrapper.value)