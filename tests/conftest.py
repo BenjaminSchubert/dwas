BenjaminSchubert/wast

Ensure defaults from predefined steps are not modified
Otherwise, if a user runs them multiple times, the actual arguments
might end up different.

tests/conftest.py
@@ -1,5 +1,6 @@
 # pylint and pytest fixtures dependency injection are not friends
 # pylint: disable=redefined-outer-name
+import copy
 import logging
 import sys
 from dataclasses import dataclass
@@ -8,9 +9,9 @@ from typing import List, Optional, Union
 import pytest
 from _pytest.capture import FDCapture, MultiCapture
 
-from wast import Config
+from wast import Config, predefined
 from wast.__main__ import main
-from wast._pipeline import Pipeline
+from wast._pipeline import Pipeline, set_pipeline
 from wast._subproc import set_subprocess_default_pipes
 
 from ._utils import isolated_context, isolated_logging
@@ -94,3 +95,53 @@ def sample_config(tmp_path):
 @pytest.fixture
 def pipeline(sample_config):
     return Pipeline(sample_config)
+
+
+@pytest.fixture(autouse=True, scope="session")
+def ensure_defaults_are_untouched(tmp_path_factory):
+    """
+    Ensure that the defaults values for predefined steps are not mutated.
+
+    The predefined steps set some defaults that might be mutable, e.g. lists.
+    We need to take special care to ensure we do not modify them at runtime so
+    that the steps are reusable.
+
+    This loads all the steps before anything runs, and deep copies the defaults.
+    It then validates that they did not change at the end of the run.
+    """
+    steps = {}
+
+    @isolated_context
+    def load_steps():
+        # Load all the steps programatically, and get their defaults.
+        # We could probably do it a bit simpler, without a pipeline, but this
+        # works without us having to do more special magic.
+        pipeline = Pipeline(
+            Config(
+                cache_path=tmp_path_factory.mktemp("defaults-validation"),
+                verbosity=2,
+                colors=False,
+                n_jobs=1,
+                skip_missing_interpreters=False,
+                skip_setup=False,
+                skip_run=False,
+                fail_fast=False,
+            )
+        )
+        set_pipeline(pipeline)
+
+        for step_factory_name in predefined.__all__:
+            steps[step_factory_name] = getattr(predefined, step_factory_name)()
+
+    def extract_defaults():
+        return {name: step.__wast_defaults__ for name, step in steps.items()}
+
+    load_steps()
+    # Deep copy the defaults
+    original_values = copy.deepcopy(extract_defaults())
+    yield
+
+    # And validate
+    assert (
+        extract_defaults() == original_values
+    ), "BUG: Defaults arguments were mutated during the tests."

BenjaminSchubert/dwas

tests/conftest.py
@@ -1,5 +1,6 @@
 # pylint and pytest fixtures dependency injection are not friends
 # pylint: disable=redefined-outer-name
+import copy
 import logging
 import sys
 from dataclasses import dataclass
@@ -8,9 +9,9 @@ from typing import List, Optional, Union
 import pytest
 from _pytest.capture import FDCapture, MultiCapture
 
-from dwas import Config
+from dwas import Config, predefined
 from dwas.__main__ import main
-from dwas._pipeline import Pipeline
+from dwas._pipeline import Pipeline, set_pipeline
 from dwas._subproc import set_subprocess_default_pipes
 
 from ._utils import isolated_context, isolated_logging
@@ -95,3 +96,53 @@ def sample_config(tmp_path):
 @pytest.fixture
 def pipeline(sample_config):
     return Pipeline(sample_config)
+
+
+@pytest.fixture(autouse=True, scope="session")
+def ensure_defaults_are_untouched(tmp_path_factory):
+    """
+    Ensure that the defaults values for predefined steps are not mutated.
+
+    The predefined steps set some defaults that might be mutable, e.g. lists.
+    We need to take special care to ensure we do not modify them at runtime so
+    that the steps are reusable.
+
+    This loads all the steps before anything runs, and deep copies the defaults.
+    It then validates that they did not change at the end of the run.
+    """
+    steps = {}
+
+    @isolated_context
+    def load_steps():
+        # Load all the steps programatically, and get their defaults.
+        # We could probably do it a bit simpler, without a pipeline, but this
+        # works without us having to do more special magic.
+        pipeline = Pipeline(
+            Config(
+                cache_path=tmp_path_factory.mktemp("defaults-validation"),
+                verbosity=2,
+                colors=False,
+                n_jobs=1,
+                skip_missing_interpreters=False,
+                skip_setup=False,
+                skip_run=False,
+                fail_fast=False,
+            )
+        )
+        set_pipeline(pipeline)
+
+        for step_factory_name in predefined.__all__:
+            steps[step_factory_name] = getattr(predefined, step_factory_name)()
+
+    def extract_defaults():
+        return {name: step.__dwas_defaults__ for name, step in steps.items()}
+
+    load_steps()
+    # Deep copy the defaults
+    original_values = copy.deepcopy(extract_defaults())
+    yield
+
+    # And validate
+    assert (
+        extract_defaults() == original_values
+    ), "BUG: Defaults arguments were mutated during the tests."