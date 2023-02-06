40b4532c232d8341d28ec821f7f55f5b034fb960
Benjamin Schubert <contact@benschubert.me>
2023-02-06 12:54:24 +0000

    When showing time durations, strip ms, it's too much precision

--- src/dwas/_timing.py
+++ src/dwas/_timing.py
@@ -4,3 +4,8 @@ from datetime import timedelta
 
 def get_timedelta_since(start: float) -> timedelta:
     return timedelta(seconds=time.monotonic() - start)
+
+
+def format_timedelta(delta: timedelta) -> str:
+    rnd = 1 if delta.microseconds > 500000 else 0
+    return str(timedelta(delta.days, delta.seconds + rnd, microseconds=0))
